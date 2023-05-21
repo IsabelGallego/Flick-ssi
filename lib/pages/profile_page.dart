e17e81e0bcbc6f1cb3572e3f6860d072d80cb725
DracoXZ0 <londonosimon67873@gmail.com>
2023-05-21 10:24:57 -0500

    Cerrar sesion

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,16 +1,30 @@
+// ignore_for_file: use_build_context_synchronously, no_leading_underscores_for_local_identifiers
+
 import 'package:flickssi/services/firebase_service.dart';
 import 'package:flutter/material.dart';
-import 'package:flickssi/constants/constants.dart';
 import 'package:flickssi/models/profile_list_tile_model.dart';
 import 'package:flickssi/widgets/icon_widget.dart';
 import 'package:flickssi/widgets/text1.dart';
 import 'package:flickssi/widgets/text2.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+  const ProfilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+    Future<void> _signOut() async {
+      try {
+        await _googleSignIn.signOut();
+
+        Navigator.pushReplacementNamed(context, '/login');
+      } catch (error) {
+        ('Error al cerrar sesión: $error');
+      }
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text1(
@@ -43,7 +57,7 @@ class ProfilePage extends StatelessWidget {
                         if (snapshot.connectionState ==
                             ConnectionState.waiting) {
                           // Mientras se carga el nombre del usuario, se muestra un indicador de carga
-                          return CircularProgressIndicator();
+                          return const CircularProgressIndicator();
                         } else {
                           if (snapshot.hasData && snapshot.data != null) {
                             // Si se obtiene el nombre del usuario correctamente, se muestra en el Text widget
@@ -51,7 +65,7 @@ class ProfilePage extends StatelessWidget {
                               children: [
                                 Text(
                                   snapshot.data!,
-                                  style: TextStyle(
+                                  style: const TextStyle(
                                     fontSize: 25,
                                     fontWeight: FontWeight.bold,
                                   ),
@@ -60,7 +74,7 @@ class ProfilePage extends StatelessWidget {
                             );
                           } else {
                             // Si no se puede obtener el nombre del usuario, se muestra un mensaje alternativo
-                            return Text(
+                            return const Text(
                               'Hola, Usuario',
                               style: TextStyle(
                                 fontSize: 25,
@@ -76,24 +90,29 @@ class ProfilePage extends StatelessWidget {
               ],
             ),
           ),
-          const Divider(),
           Expanded(
-            child: SizedBox(
-              height: double.maxFinite,
-              child: ListView.builder(
-                  itemCount: profileListTile.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      leading: IconWidget(
-                        iconPath: profileListTile[index].iconPath,
-                      ),
-                      title: Text1(
-                        text: profileListTile[index].title,
-                        isBold: false,
-                      ),
-                      subtitle: Text2(text: profileListTile[index].subtitle),
-                    );
-                  }),
+            child: ListView.builder(
+              itemCount: profileListTile.length,
+              itemBuilder: (context, index) {
+                final tile = profileListTile[index];
+                return ListTile(
+                  leading: IconWidget(
+                    iconPath: tile.iconPath,
+                  ),
+                  title: Text1(
+                    text: tile.title,
+                    isBold: false,
+                  ),
+                  subtitle: Text2(text: tile.subtitle),
+                  onTap: () {
+                    if (tile.title == 'Cerrar sesión') {
+                      _signOut(); // Llama a la función _signOut para cerrar sesión
+                    } else {
+                      // Lógica para manejar otros elementos de la lista
+                    }
+                  },
+                );
+              },
             ),
           ),
         ],
